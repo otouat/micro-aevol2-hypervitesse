cf754cd82f532a8e912cdfcfa68115ff5e74ce42
Martin <mfranc.martin@gmail.com>
2021-01-17 02:08:38 +0100

    le script python utilise la version release

--- Benchmarking/Constants.py
+++ Benchmarking/Constants.py
@@ -4,33 +4,11 @@ from pathlib import Path
 CURRENT_FOLDER = Path().absolute()  # Current folder
 PROJECT_HOME_FOLDER = CURRENT_FOLDER.parent  # Project root folder
 HAS_TRACE_CSV_FILE = True
+EXEC_FILE = PROJECT_HOME_FOLDER / "cmake-build-release" / "micro_aevol_cpu"
 TRACE_CSV_FILE = CURRENT_FOLDER / "trace.csv"
 STATS_BEST_CSV_FILE = CURRENT_FOLDER / "stats" / "stats_simd_best.csv"
 STATS_MEAN_CSV_FILE = CURRENT_FOLDER / "stats" / "stats_simd_mean.csv"
-ENABLE_TRACES_CSV_IF_RELEASE = False  # On Release, if True, then the "traces.csv" file is expected.
-FORCE_RELEASE = True  # If true then we always go for Release.
 
-
-def _get_most_recent_exec_file() -> Path:
-    debug_file = PROJECT_HOME_FOLDER / "cmake-build-debug" / "micro_aevol_cpu"
-    release_file = PROJECT_HOME_FOLDER / "cmake-build-release" / "micro_aevol_cpu"
-
-    debug_mtime = debug_file.stat().st_mtime
-    release_mtime = release_file.stat().st_mtime
-
-    if debug_mtime <= release_mtime or FORCE_RELEASE:
-        # Return release exec file if most recent, and kill "traces.csv" data.
-        if not ENABLE_TRACES_CSV_IF_RELEASE:
-            global HAS_TRACE_CSV_FILE, TRACE_CSV_FILE
-            HAS_TRACE_CSV_FILE = False
-            # del TRACE_CSV_FILE
-        return release_file
-    else:
-        # Return debug exec file if most recent.
-        return debug_file
-
-
-EXEC_FILE = _get_most_recent_exec_file()
 
 FIXED_SEED = 42  # Always give that seed to the program.
 FIRST_CHECKS_NBR_STEPS = 3
